7fe661ecd4385baae031f304b03ae44e9414ec11
kurtreyn <kurtreyn@yahoo.com>
2022-03-01 23:15:00 -0500

    finish registration functionality

--- components/auth/Register.js
+++ components/auth/Register.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,51 +7,104 @@ import {
   TextInput,
   Alert,
   TouchableOpacity,
+  KeyboardAvoidingView,
 } from 'react-native';
 import { auth } from '../../src/firebase';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Register = ({ navigate }) => {
-  const nameRef = useRef();
-  const emailRef = useRef();
-  const passwordRef = useRef();
-  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  function registerUser(email, password) {
+  function signupUser(email, password) {
     return createUserWithEmailAndPassword(auth, email, password);
   }
 
-  async function onSignup() {
-    setLoading(true);
-
+  async function handleSignUp() {
     try {
-      await registerUser(emailRef.current.value, passwordRef.current.value);
-      Alert.alert('Registration was successful');
+      await signupUser(email, password);
     } catch (error) {
       console.log(error);
     }
-
-    setLoading(false);
   }
 
   return (
-    <View>
-      <TextInput placeholder="name" autoCapitalize="none" ref={nameRef} />
-      <TextInput placeholder="email" autoCapitalize="none" ref={emailRef} />
-      <TextInput
-        placeholder="password"
-        autoCapitalize="none"
-        secureTextEntry={true}
-        ref={passwordRef}
-      />
-      <TouchableOpacity onPress={onSignup}>
-        <Button title="Sign Up" />
-      </TouchableOpacity>
-    </View>
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          autoCapitalize="none"
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
+        >
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default Register;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
+import Config from 'react-native-config';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyC4tiwYUGt0iFFHGWlUUWjGbQzFWJZX2zs',
   authDomain: 'ig-clone-60973.firebaseapp.com',
@@ -9,12 +11,13 @@ const firebaseConfig = {
   storageBucket: 'ig-clone-60973.appspot.com',
   messagingSenderId: '623175335938',
   appId: '1:623175335938:web:c3654cb050c7e08843a71b',
-  //   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  //   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  //   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  //   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+
+  // apiKey: REACT_NATIVE_APP_FIREBASE_API_KEY,
+  // authDomain: REACT_NATIVE_APP_FIREBASE_AUTH_DOMAIN,
+  // projectId: REACT_NATIVE_APP_FIREBASE_PROJECT_ID,
+  // storageBucket: REACT_NATIVE_APP_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: REACT_NATIVE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: REACT_NATIVE_APP_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
